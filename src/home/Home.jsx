6c4a87801c07125b7686495ad427f300f92b2242
John <jddhalogamer@gmail.com>
2023-02-26 18:17:21 -0600

    added conditional to navbar for responsive styling.

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,6 +4,11 @@ import Navbar from '../components/navbar/Navbar.jsx'
 const Home = () => {
   return (
     <div className="home">
+      <img
+      width='100%'
+      height='100%' 
+      src='https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+      alt="" />
       <Navbar />
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae vel quia, odio odit voluptatibus repellendus deleniti accusantium magni porro? Maiores deserunt numquam debitis maxime, quaerat eius nostrum explicabo hic cum.
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae vel quia, odio odit voluptatibus repellendus deleniti accusantium magni porro? Maiores deserunt numquam debitis maxime, quaerat eius nostrum explicabo hic cum.
