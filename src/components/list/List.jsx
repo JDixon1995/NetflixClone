29bf125681ce0a947eb2c4131920e2a309c2a57d
John <jddhalogamer@gmail.com>
2023-02-27 20:04:58 -0600

    added animation to List arrows to move rows of content.

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,34 @@
 import './list.scss'
 import { ArrowForwardIosOutlined, ArrowBackIosOutlined } from '@material-ui/icons'
 import ListItem from '../listItem/ListItem.jsx'
+import { useRef } from 'react'
 
 const List = () => {
+
+    const listRef = useRef()
+
+    const handleClick = (direction) => {
+
+        let distance = listRef.current.getBoundingClientRect().x - 50
+        console.log(distance)
+
+        if(direction === 'left') {
+            listRef.current.style.transform = `translateX(${230 + distance}px)`
+        }
+
+        if(direction === 'right') {
+            listRef.current.style.transform = `translateX(${-230 + distance}px)`
+        }
+    }
+
   return (
     <div className="list">
         <span className="listTitle">Continue to Watch</span>
         <div className="wrapper">
-            <ArrowBackIosOutlined className="sliderArrow left" />
-            <div className="container">
+            <ArrowBackIosOutlined 
+            className="sliderArrow left"
+            onClick={() => handleClick('left')} />
+            <div className="container" ref={listRef}>
                 <ListItem />
                 <ListItem />
                 <ListItem />
@@ -17,7 +37,9 @@ const List = () => {
                 <ListItem />
                 <ListItem />
             </div>
-            <ArrowForwardIosOutlined className="sliderArrow right" />
+            <ArrowForwardIosOutlined 
+            className="sliderArrow right"
+            onClick={() => handleClick('right')} />
         </div>
     </div>
   )
