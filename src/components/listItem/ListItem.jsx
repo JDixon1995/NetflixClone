10e5687cdd8625f9a9ee7558594d9deedb0ea075
John <jddhalogamer@gmail.com>
2023-03-02 18:54:52 -0600

    added thumbnail and hover styling to slider items.

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,21 @@
 import './listItem.scss'
+import { PlayArrow, Add, ThumbUp, ThumbDown } from '@material-ui/icons'
 
 const ListItem = () => {
   return (
     <div className="listItem">
-        item
+        <img 
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" 
+        alt="" 
+        />
+        <div className="itemInfo">
+          <div className="icons">
+            <PlayArrow />
+            <Add />
+            <ThumbUp />
+            <ThumbDown />
+          </div>
+        </div>
     </div>
   )
 }
