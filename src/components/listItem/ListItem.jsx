5e2f40b2ea7d7df415b9ae344a7133ef64d078b3
John <jddhalogamer@gmail.com>
2023-03-07 18:18:24 -0600

    added mouse hover trailer video to thumbnails.

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -18,26 +18,30 @@ const ListItem = ({ index}) => {
         src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" 
         alt="" 
         />
-        <video 
-        src={trailer} 
-        autoPlay={true}
-        loop
-        />
-        <div className="itemInfo">
-          <div className="icons">
-            <PlayArrow />
-            <Add />
-            <ThumbUp />
-            <ThumbDown />
+        { isHovered && (
+          <>
+          <video 
+          src={trailer} 
+          autoPlay={true}
+          loop
+          />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow />
+              <Add />
+              <ThumbUp />
+              <ThumbDown />
+            </div>
+            <div className="intemInfoTop">
+              <span>1 Hour, 14 Minutes</span>
+              <span className="limit">+17</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore consectetur maxime laborum porro quisquam reiciendis mollitia.</div>
           </div>
-          <div className="intemInfoTop">
-            <span>1 Hour, 14 Minutes</span>
-            <span className="limit">+17</span>
-            <span>1999</span>
-          </div>
-          <div className="desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore consectetur maxime laborum porro quisquam reiciendis mollitia.</div>
-        </div>
-        <div className="genre">Action</div>
+          <div className="genre">Action</div>
+          </>
+        )}
     </div>
   )
 }
