f418bac033c0a90fd9dc31d3e143e0b5b49b62c9
John <jddhalogamer@gmail.com>
2023-03-09 18:51:24 -0600

    wrote basic page for Register component.

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,25 @@ import './register.scss'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register">
+        <div className="top">
+            <img 
+            className='logo'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png' 
+            alt="" 
+            />
+            <button className="loginButton">Sign In</button>
+        </div>
+        <div className="container">
+            <h1>Unlimited movies, TV shows, and more.</h1>
+            <h2>Watch Anywhere. Cancel anytime.</h2>
+            <p>Ready to watch? Enter your email to create or restart your membership.</p>
+            <div className="input">
+                <input type="email" placeholder='email address' />
+                <button className="registerButton">Get Started</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
