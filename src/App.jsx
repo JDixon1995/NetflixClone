216574ba6978177b570819dc3d4fb66f066f1c49
John <jddhalogamer@gmail.com>
2023-03-11 10:42:16 -0600

    built Login component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import './app.scss'
 import Home from './pages/home/Home'
 import Watch from './pages/watch/Watch';
 import Register from './pages/register/Register';
+import Login from './pages/login/Login'
 
 function App() {
-  return <Register />
+  return <Login />
 }
 
 export default App;
