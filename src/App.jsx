96a2e53a035e15835d81f73bc26ddab33ddca30a
John <jddhalogamer@gmail.com>
2023-03-22 09:01:53 -0500

    added React Router to client.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import './app.scss'
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+ } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Watch from './pages/watch/Watch';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login'
 
 function App() {
-  return <Login />
+  return(
+    <Router>
+      <Routes>
+        <Route
+        exact 
+        path='/'
+        element={<Home />}
+        />
+        <Route 
+        path='/movies'
+        element={<Home type='movies' />}
+        /> 
+        <Route />
+        <Route 
+        path='/series'
+        element={<Home type='series' />}
+        /> 
+        <Route />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,11 @@ import Navbar from '../../components/navbar/Navbar.jsx'
 import FeaturedMovie from '../../components/featuredMovie/FeaturedMovie.jsx'
 import List from '../../components/list/List.jsx'
 
-const Home = () => {
+const Home = ({ type }) => {
   return (
     <div className="home">
       <Navbar />
-      <FeaturedMovie />
+      <FeaturedMovie type={type} />
       <List />
       <List />
       <List />
