878d08590e8256de688424aee7fea47330421fec
John <jddhalogamer@gmail.com>
2023-03-23 18:20:26 -0500

    added useState and axios to Home component.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,28 @@ import './home.scss'
 import Navbar from '../../components/navbar/Navbar.jsx'
 import FeaturedMovie from '../../components/featuredMovie/FeaturedMovie.jsx'
 import List from '../../components/list/List.jsx'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Home = ({ type }) => {
+
+  const [ lists, setLists ] = useState([])
+  const [ genre, setGenre ] = useState(null)
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type && '?type=' + type}&${genre && 'genre=' + genre}`)
+          setLists(res.data)
+          console.log(lists)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getRandomLists()
+  }, [type, genre])
+
   return (
     <div className="home">
       <Navbar />
