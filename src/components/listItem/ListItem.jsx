4e16d4342ce2269a07822a626af414483b50e1c6
John <jddhalogamer@gmail.com>
2023-03-25 15:10:44 -0500

    thumbnails in lists now display object data.

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,11 +1,26 @@
 import './listItem.scss'
 import { PlayArrow, Add, ThumbUp, ThumbDown } from '@material-ui/icons'
-import { useState } from 'react'
-const ListItem = ({ index}) => {
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+const ListItem = ({ index, item }) => {
 
   const [ isHovered, setIsHovered ] = useState(false)
-  const trailer = 
-  "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+  const [ movie, setMovie ] = useState({})
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const res = await axios.get('/movies/find/' + item, {
+          headers : {
+            "token" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MThlMzJjM2RlYzI2M2IwNzMwZjU4ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3OTc2ODgxMSwiZXhwIjoxNjgwMjAwODExfQ.u0udN9ucvlN2-OzwMllypjiJiOO_Cg6IB-8FWFgwPdk"
+          },
+        })
+        setMovie(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getMovie()
+  }, [item])
 
   return (
     <div 
@@ -15,13 +30,13 @@ const ListItem = ({ index}) => {
     onMouseLeave={() => {setIsHovered(false)}}
     >
         <img 
-        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" 
+        src={movie.img}
         alt="" 
         />
         { isHovered && (
           <>
           <video 
-          src={trailer} 
+          src={movie.trailer} 
           autoPlay
           loop
           muted
@@ -36,11 +51,11 @@ const ListItem = ({ index}) => {
             <div className="itemInfoTop">
               <span>1 Hour, 14 Minutes</span>
               <span className="limit">+17</span>
-              <span>1999</span>
+              <span>{movie.year}</span>
             </div>
-            <div className="desc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore consectetur maxime laborum porro quisquam reiciendis mollitia.</div>
+            <div className="desc">{movie.desc}</div>
           </div>
-          <div className="genre">Action</div>
+          <div className="genre">{movie.genre}</div>
           </>
         )}
     </div>
